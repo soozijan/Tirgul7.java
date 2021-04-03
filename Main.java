3dcb9c72c0822fcd4bff1ec900377f9710634448
soozijan <75090137+soozijan@users.noreply.github.com>
2021-04-03 21:39:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
  //שאלה 1
         LocalDateTime myDate = LocalDateTime.of(1984, 4, 15,7,00);
         System.out.println(myDate);
+
+//שאלה 3
+        ArrayList<Continent> continentArrayList = new ArrayList<>();
+       continentArrayList.add(new Continent("Africa", 100000004, 4));
+        continentArrayList.add(new Continent("America", 100008754, 2));
+        Collections.sort(continentArrayList,new ContinentSortByName());
+        System.out.println(continentArrayList);
+
