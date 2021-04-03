c2e357433d2a65fb1fb107017b8be6f279e59997
soozijan <75090137+soozijan@users.noreply.github.com>
2021-04-03 21:44:12 +0300

    Create ComtinentSortByPopulation.java

---
+++ ComtinentSortByPopulation.java
@@ -0,0 +1,18 @@
+package Tirgul7;
+
+import java.util.Comparator;
+
+public class ComtinentSortByPopulation implements Comparator<Continent> {
+    @Override
+    public int compare(Continent o1,Continent o2) {
+
+       if (o1.m_population > o2.m_population)   {
+           return 1;
+       }
+       else if (o1.m_population < o2.m_population)   {
+           return -1;
+}
+       return 0;
+
+}
+}
